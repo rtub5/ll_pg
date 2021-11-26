25e72c408ef458e112c31dd39f43e1a7d48b2115
rtub5 <77343357+rtub5@users.noreply.github.com>
2021-11-26 10:27:52 +0100

    Update tir parabolic.py

--- tir parabolic.py
+++ tir parabolic.py
@@ -1,7 +1,4 @@
-#tir parabolic
-#htop 
-#abast 
-import matplotlib.pyplot as plt
+import matplotlib as plt
 
 x0 = float(input("x0: "))
 y0 = float(input("y0: "))
@@ -20,19 +17,22 @@ coordenada_y_ = []
 
 while True:
     
-     
+    temps += 0.0001 
     coordenades___y = float(y0 + vy0 * temps - gravetat_p * temps**2) 
     coordenades___x = float(x0 + vx0 * temps )
     coordenada_y_.append(coordenades___y)
     coordenada_x.append(coordenades___x)
-    temps += 0.0001
+    
 
     if coordenada_y_[-1] <= 0:
         break
 
+
+print(coordenada_y_[-1])
+print(coordenada_x[-1])
+print(temps)
+
 plt.style.use("bmh")
 plt.scatter(coordenada_x, coordenada_y_)
 plt.show() 
-plt.scatter(coordenada_x, coordenada_y_)
 
-#def mort(): #calcul probabilitat de mort
